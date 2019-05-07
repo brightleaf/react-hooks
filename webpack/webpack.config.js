0ecbe0db80aed56a091514829aad64e957252cd5
Kevin Isom <kevin.isom@gmail.com>
2019-05-08 09:18:03 +1200

    webpack config work working

---
+++ webpack/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './working/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(mjs|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ca]ss$/,
+        loader: 'style-loader!css-loader!resolve-url-loader!sass-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: './working/index.html',
+      historyApiFallback: true,
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './src/public',
+    hot: true,
+  },
+  resolve: {
+    extensions: ['*', '.mjs', '.js', '.jsx'],
+    modules: ['node_modules', 'src'],
+    alias: {},
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+}
