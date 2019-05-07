34020776c8992b573098ad880fa3d157fd7f1a0a
Kevin Isom <kevin.isom@gmail.com>
2019-05-08 08:40:18 +1200

    Reducer for http requests

---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case 'get':
+      return { ...state, loading: true }
+    case 'success':
+      return {
+        ...state,
+        data: action.payload.data,
+        error: null,
+        loading: false,
+      }
+    case 'error':
+      return {
+        ...state,
+        data: null,
+        error: action.payload.error,
+        loading: false,
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
