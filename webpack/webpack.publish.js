a01fffd3c91e77aa59031000fcbb668e8f477e27
Kevin Isom <kevin.isom@gmail.com>
2019-05-08 17:12:21 +1200

    publish config - not used currently

---
+++ webpack/webpack.publish.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(mjs|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ca]ss$/,
+        loader: 'style-loader!css-loader!resolve-url-loader!sass-loader',
+      },
+    ],
+  },
+  plugins: [],
+
+  resolve: {
+    extensions: ['*', '.mjs', '.js', '.jsx'],
+    modules: ['node_modules', 'src'],
+    alias: {},
+  },
+  output: {
+    path: path.join(__dirname, '/lib'),
+    filename: 'index.js',
+  },
+}
