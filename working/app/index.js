6d402dee1fbecd061927e0b232067bb62b705f4a
Kevin Isom <kevin.isom@gmail.com>
2019-05-09 16:31:16 +1200

    links

--- working/app/index.js
+++ working/app/index.js
@@ -6,6 +6,7 @@ const Graph = React.lazy(() => import('../hooks/graph'))
 const Grapher = React.lazy(() => import('../hooks/grapher'))
 const Getter = React.lazy(() => import('../hooks/getter'))
 const Poster = React.lazy(() => import('../hooks/poster'))
+const Nes = React.lazy(() => import('../hooks/nes'))
 export default class App extends Component {
   render() {
     return (
@@ -14,13 +15,14 @@ export default class App extends Component {
           <Link to="/">Fetch</Link> | <Link to="/getter">Get</Link> |{' '}
           <Link to="/graph">GraphQL</Link> |{' '}
           <Link to="/grapher">GraphQL Vars</Link> |{' '}
-          <Link to="/poster">Poster</Link>
+          <Link to="/poster">Poster</Link> | <Link to="/nes">Nes</Link>
           <Router>
             <Fetch path="/" />
             <Getter path="/getter" />
             <Graph path="/graph" />
             <Grapher path="/grapher" />
             <Poster path="/poster" />
+            <Nes path="/nes" />
           </Router>
         </React.Suspense>
       </Fragment>
