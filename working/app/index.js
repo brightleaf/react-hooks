f49bb1dac29c19d66b30be5fd83554d543f77401
Kevin Isom <kevin.isom@gmail.com>
2019-05-16 10:36:01 +1200

    Use hash for routes for ghpages

--- working/app/index.js
+++ working/app/index.js
@@ -1,33 +1,38 @@
 import React, { Component, Fragment } from 'react'
-import { Router, Link } from '@reach/router'
-
+import { Router, createHistory, Link, LocationProvider } from '@reach/router'
+import createHashSource from 'hash-source'
 const Fetch = React.lazy(() => import('../hooks/fetcher'))
 const Graph = React.lazy(() => import('../hooks/graph'))
 const Grapher = React.lazy(() => import('../hooks/grapher'))
 const Getter = React.lazy(() => import('../hooks/getter'))
 const Poster = React.lazy(() => import('../hooks/poster'))
 const Nes = React.lazy(() => import('../hooks/nes'))
+
+const source = createHashSource()
+const history = createHistory(source)
+
 export default class App extends Component {
   render() {
     return (
-      <Fragment>
-        <React.Suspense fallback={<div>Loading</div>}>
-          <Link to="/react-hooks/examples">Fetch</Link> |{' '}
-          <Link to="/getter">Get</Link> |{' '}
-          <Link to="/react-hooks/examples/graph">GraphQL</Link> |{' '}
-          <Link to="/react-hooks/examples/grapher">GraphQL Vars</Link> |{' '}
-          <Link to="/react-hooks/examples/poster">Poster</Link> |{' '}
-          <Link to="/react-hooks/examples/nes">Nes</Link>
-          <Router>
-            <Fetch path="/react-hooks/examples" />
-            <Getter path="/react-hooks/examples/getter" />
-            <Graph path="/react-hooks/examples/graph" />
-            <Grapher path="/react-hooks/examples/grapher" />
-            <Poster path="/react-hooks/examples/poster" />
-            <Nes path="/react-hooks/examples/nes" />
-          </Router>
-        </React.Suspense>
-      </Fragment>
+      <LocationProvider history={history}>
+        <Fragment>
+          <React.Suspense fallback={<div>Loading</div>}>
+            <Link to="examples">Fetch</Link> | <Link to="getter">Get</Link> |{' '}
+            <Link to="examples/graph">GraphQL</Link> |{' '}
+            <Link to="examples/grapher">GraphQL Vars</Link> |{' '}
+            <Link to="examples/poster">Poster</Link> |{' '}
+            <Link to="examples/nes">Nes</Link>
+            <Router>
+              <Fetch path="examples" />
+              <Getter path="examples/getter" />
+              <Graph path="examples/graph" />
+              <Grapher path="examples/grapher" />
+              <Poster path="examples/poster" />
+              <Nes path="examples/nes" />
+            </Router>
+          </React.Suspense>
+        </Fragment>
+      </LocationProvider>
     )
   }
 }
