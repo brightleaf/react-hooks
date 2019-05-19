7ba75531c809746d6aceca1073fdb20141af85b4
Kevin Isom <kevin.isom@gmail.com>
2019-05-20 07:41:21 +1200

    First cut of use mutation

---
+++ src/use-mutation.js
@@ -0,0 +1,44 @@
+import { request } from 'graphql-request'
+import { useReducer } from 'react'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'get':
+      return { ...state, loading: true }
+    case 'success':
+      return {
+        ...state,
+        data: action.payload.data,
+        error: null,
+        loading: false,
+      }
+    case 'error':
+      return {
+        ...state,
+        data: null,
+        error: action.payload.error,
+        loading: false,
+      }
+    default:
+      return state
+  }
+}
+
+const useMutation = function(url, query, variables) {
+  const [state, dispatch] = useReducer(reducer, {
+    data: [],
+    error: null,
+    loading: true,
+  })
+  const makeQuery = async variables => {
+    dispatch({ type: 'get' })
+    const resp = await request(url, query, variables)
+    const data = resp
+    dispatch({ type: 'success', payload: { data } })
+  }
+
+  return { ...state, makeQuery }
+}
+
+export { useMutation as useGraphQL }
+export default useMutation
