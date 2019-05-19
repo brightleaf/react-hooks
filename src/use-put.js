600fad691899d7498a68f5fbb5e3fa67e36000c1
Kevin Isom <kevin.isom@gmail.com>
2019-05-19 19:05:27 +1200

    quick and dirty put

---
+++ src/use-put.js
@@ -0,0 +1,32 @@
+import { useReducer } from 'react'
+import { reducer } from './reducer'
+
+export const usePost = (url = '') => {
+  const [state, dispatch] = useReducer(reducer, {
+    data: [],
+    error: null,
+    loading: false,
+  })
+
+  const postData = async data => {
+    dispatch({ type: 'get' })
+    const resp = await fetch(url, {
+      method: 'PUT',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrer: 'no-referrer',
+      body: JSON.stringify(data),
+    })
+    const result = await resp.json()
+    dispatch({ type: 'success', payload: { data: result } })
+  }
+
+  return { ...state, postData }
+}
+
+export default usePost
