a174e6f3c0a2499b0d3bfa9d359c2f652f70a58b
Kevin Isom <kevin.isom@gmail.com>
2019-06-22 09:45:33 +1200

    Add use-icon and use-title

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import useAsync from './use-async'
 import useClickOutside from './use-click-outside'
+import useFavicon from './use-favicon'
 import useFetch from './use-fetch'
 import useGet from './use-get'
 import useGraphQL from './use-graphql'
@@ -8,11 +9,13 @@ import useMutation from './use-mutation'
 import useNes from './use-nes'
 import usePost from './use-post'
 import usePut from './use-put'
+import useTitle from './use-title'
 import useWebSockets from './use-websockets'
 
 export {
   useAsync,
   useClickOutside,
+  useFavicon,
   useFetch,
   useGet,
   useGraphQL,
@@ -21,5 +24,6 @@ export {
   useNes,
   usePost,
   usePut,
+  useTitle,
   useWebSockets,
 }
---
+++ src/use-title.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+
+const useTitle = title => {
+  useEffect(() => {
+    document.title = title
+  }, [title])
+}
+
+export default useTitle
+
+export { useTitle }
