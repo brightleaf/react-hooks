4a72657070dba753e325d42f0d20dcaa01562ff8
Kevin Isom <kevin.isom@gmail.com>
2019-06-22 17:26:28 +1200

    tabs in example

--- working/app/index.js
+++ working/app/index.js
@@ -1,5 +1,11 @@
 import React, { Component, Fragment } from 'react'
-import { Router, createHistory, Link, LocationProvider } from '@reach/router'
+import {
+  Router,
+  createHistory,
+  Link,
+  LocationProvider,
+  Location,
+} from '@reach/router'
 import createHashSource from 'hash-source'
 import Intro from './intro'
 import Home from './home'
@@ -13,29 +19,59 @@ const Local = React.lazy(() => import('../hooks/local'))
 const source = createHashSource()
 const history = createHistory(source)
 
+const TabLink = props => {
+  console.log('TabLink', props)
+  return (
+    <Location>
+      {({ location }) => {
+        const active = props.to === location.pathname
+        return (
+          <li className={active ? 'is-active' : ''}>
+            <Link
+              {...props}
+              getProps={prop => {
+                console.info('link prop', prop)
+                const { isCurrent } = prop
+                return {
+                  className: isCurrent ? 'is-active' : '',
+                }
+              }}
+            />
+          </li>
+        )
+      }}
+    </Location>
+  )
+}
+
 export default class App extends Component {
   render() {
     return (
       <LocationProvider history={history}>
         <Fragment>
           <React.Suspense fallback={<div>Loading</div>}>
-            <Link to="/">Home</Link> | <Link to="examples">Fetch</Link> |{' '}
-            <Link to="examples/getter">Get</Link> |{' '}
-            <Link to="examples/graph">GraphQL</Link> |{' '}
-            <Link to="examples/grapher">GraphQL Vars</Link> |{' '}
-            <Link to="examples/poster">Poster</Link> |{' '}
-            <Link to="examples/nes">Nes</Link> |{' '}
-            <Link to="examples/local">Local Storage</Link>
+            <div className="tabs">
+              <ul>
+                <TabLink to="/">Home</TabLink>
+                <TabLink to="/examples">Fetch</TabLink>
+                <TabLink to="/examples/getter">Get</TabLink>
+                <TabLink to="/examples/graph">GraphQL</TabLink>
+                <TabLink to="/examples/grapher">GraphQL Vars</TabLink>
+                <TabLink to="/examples/poster">Poster</TabLink>
+                <TabLink to="/examples/nes">Nes</TabLink>
+                <TabLink to="/examples/local">Local Storage</TabLink>
+              </ul>
+            </div>
             <Intro />
             <Router>
               <Home path="/" />
-              <Fetch path="examples" />
-              <Getter path="examples/getter" />
-              <Graph path="examples/graph" />
-              <Grapher path="examples/grapher" />
-              <Poster path="examples/poster" />
-              <Nes path="examples/nes" />
-              <Local path="examples/local" />
+              <Fetch path="/examples" />
+              <Getter path="/examples/getter" />
+              <Graph path="/examples/graph" />
+              <Grapher path="/examples/grapher" />
+              <Poster path="/examples/poster" />
+              <Nes path="/examples/nes" />
+              <Local path="/examples/local" />
             </Router>
           </React.Suspense>
         </Fragment>
