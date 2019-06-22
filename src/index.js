989f2bfcd82b882306f88639baf4b7693df0e316
Kevin Isom <kevin.isom@gmail.com>
2019-06-22 18:37:08 +1200

    keypress hook

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import useFavicon from './use-favicon'
 import useFetch from './use-fetch'
 import useGet from './use-get'
 import useGraphQL from './use-graphql'
+import useKeypress from './use-keypress'
 import useLocalStorage from './use-local-storage'
 import useMutation from './use-mutation'
 import useNes from './use-nes'
@@ -23,6 +24,7 @@ export {
   useFetch,
   useGet,
   useGraphQL,
+  useKeypress,
   useLocalStorage,
   useMutation,
   useNes,
---
+++ src/use-keypress.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * useKeypress Hook to allow setting of the page title
+ *
+ * @param {string} key - The text to be used to set the page title
+ * @param {HTMLElement} element - The element to listen on for the keypress
+ */
+const useKeypress = (key, element = window) => {
+  const [pressed, setPressed] = useState(false)
+
+  const onKeyDownEventHandler = e => {
+    if (e.key === key) {
+      setPressed(true)
+    }
+  }
+  const onKeyUpEventHandler = e => {
+    if (e.key === key) {
+      setPressed(false)
+    }
+  }
+  useEffect(() => {
+    element.addEventListener('keydown', onKeyDownEventHandler)
+    element.addEventListener('keyup', onKeyUpEventHandler)
+    return () => {
+      element.removeEventListener('keydown', onKeyDownEventHandler)
+      element.removeEventListener('keyup', onKeyUpEventHandler)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key, element])
+
+  return pressed
+}
+
+export default useKeypress
+
+export { useKeypress }
