c02fdc7c7bf8dc3d935d1c0bcb5e9f5063d06a1e
Kevin Isom <kevin.isom@gmail.com>
2019-07-20 18:45:47 +1200

    allow updating of favicon

--- src/use-favicon.js
+++ src/use-favicon.js
@@ -1,15 +1,24 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
+/**
+ * useFavicon Hook to allow setting of the favicon of a page
+ *
+ * @param {string} href - The href of the favicon
+ * @returns {[string, function]} - The favicon href and setFavicon function to update favicon
+ */
 const useFavicon = href => {
+  const [favicon, setFavicon] = useState(href)
   useEffect(() => {
     const link =
       document.querySelector("link[rel*='icon']") ||
       document.createElement('link')
     link.type = 'image/x-icon'
     link.rel = 'shortcut icon'
-    link.href = href
+    link.href = favicon
     document.getElementsByTagName('head')[0].appendChild(link)
-  }, [href])
+  }, [favicon])
+
+  return [favicon, setFavicon]
 }
 
 export default useFavicon
