fff7d24d8d0557613f14c7bfc13595cb52a9addc
Kevin Isom <kevin.isom@gmail.com>
2019-07-22 08:01:58 +1200

    remove media match

--- working/hooks/online-status.js
+++ working/hooks/online-status.js
@@ -7,18 +7,13 @@ import {
 export default () => {
   useTitle('useOnlineStatus example from @brightleaf/react-hooks')
   const { offline, online } = useOnlineStatus()
-  const { matches } = useMediaQuery('(min-width: 500px)')
+
   return (
     <div className="App content">
       <h3>
         {offline && 'Connection is offline'}
         {online && 'Connection is online'}
       </h3>
-      {matches && (
-        <article className="message is-warning">
-          <div className="message-body">The page is at least 500px</div>
-        </article>
-      )}
       {offline && (
         <article className="message is-danger">
           <div className="message-body">Connection is offline </div>
