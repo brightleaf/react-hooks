2db458633c463edccc6d14ca347c3d6cd0d9de19
Kevin Isom <kevin.isom@gmail.com>
2019-07-22 08:02:18 +1200

    title and subtitle

--- working/app/intro.js
+++ working/app/intro.js
@@ -3,9 +3,8 @@ import React from 'react'
 export default function Intro() {
   return (
     <div>
-      <br />
-      <h1>Brightleaf React Hooks</h1>
-      <div>Useful React Hooks</div>
+      <h1 className="title">Brightleaf React Hooks</h1>
+      <p className="subtitle">Useful React Hooks</p>
       <hr />
     </div>
   )
