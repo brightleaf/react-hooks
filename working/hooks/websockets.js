632c1a2201318b8f24ce3b1a8f4924b3257c1b51
Kevin Isom <kevin.isom@gmail.com>
2019-09-17 16:19:58 +1200

    websockets demo

---
+++ working/hooks/websockets.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useWebSocket, useTitle } from '@brightleaf/react-hooks'
+export default () => {
+  useTitle('useWebSocket example from @brightleaf/react-hooks')
+
+  const { message, error, connecting, connected, sendMessage } = useWebSocket(
+    'wss://wskev.herokuapp.com'
+  )
+
+  if (error) {
+    return (
+      <div className="App">
+        <h2>Error Getting Data</h2>
+      </div>
+    )
+  }
+  const connectMessage = connecting ? (
+    <div className="alert alert-warning">Connecting</div>
+  ) : (
+    <div className="alert alert-info">Not Connecting</div>
+  )
+  const connectedMessage = connected ? (
+    <div className="alert alert-success">Connected</div>
+  ) : (
+    <div className="alert alert-danger">Not Connected</div>
+  )
+  const messages = message.map((m, index) => (
+    <div
+      className={`mar-0 pad-b-xs ${
+        index % 2 ? 'text-secondary' : 'text-primary'
+      }`}
+      key={`key-${index}`}
+    >
+      {m}
+    </div>
+  ))
+  return (
+    <div className="App content">
+      <h2>Messages from Server</h2>
+      <div>{messages}</div>
+      <div className="pad-t-md mar-t-md">
+        <div
+          className="button"
+          onClick={e => {
+            sendMessage(`This is a test message ${new Date()}`)
+          }}
+        >
+          Send a message from server
+        </div>
+      </div>
+      <br />
+      <div>
+        {connectMessage}
+        {connectedMessage}
+      </div>
+    </div>
+  )
+}
