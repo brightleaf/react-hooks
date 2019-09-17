d39da3ef0aefa94f37dbf9787a52117a55fc3019
Kevin Isom <kevin.isom@gmail.com>
2019-09-17 15:57:36 +1200

    data picking and errors

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,17 @@
+export const getData = obj => {
+  if (obj && obj.data && obj.data.data) return obj.data.data
+  if (obj && obj.data) return obj.data
+  if (obj.data === null) return null
+  return obj
+}
+export const getError = obj => {
+  if (obj && obj.error && obj.error.Error) return obj.error.Error
+  if (obj && obj.error && obj.error.error) return obj.error.error
+  if (obj && obj.error) return obj.error
+  if (obj && obj.errorMessage) return obj.data
+
+  return obj
+}
 export const reducer = (state, action) => {
   switch (action.type) {
     case 'get':
@@ -15,8 +29,9 @@ export const reducer = (state, action) => {
     case 'error':
       return {
         ...state,
-        data: null,
-        error: action.payload.error,
+        data: getData(action.payload),
+        error: true,
+        errorDetails: getError(action.payload),
         loading: false,
         complete: true,
       }
