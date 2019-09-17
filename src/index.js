cb603b79a59e3eec22d12c8d13348f32ec8f4f1c
Kevin Isom <kevin.isom@gmail.com>
2019-09-17 16:19:29 +1200

    expose the correct websocket hook, the new keypressed hook, and the useRequest hook

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import useFavicon from './use-favicon'
 import useFetch from './use-fetch'
 import useGet from './use-get'
 import useGraphQL, { useQuery } from './use-graphql'
+import useKeypressed from './use-keypressed'
 import useKeypress from './use-keypress'
 import useLocalStorage from './use-local-storage'
 import useMediaQuery from './use-media-query'
@@ -15,11 +16,12 @@ import useOnlineStatus from './use-online-status'
 import usePageVisibility from './use-page-visibility'
 import usePost from './use-post'
 import usePut from './use-put'
+import useRequest from './use-request'
 import useScript from './use-script'
 import useStyles from './use-styles'
 import useStyleSheet from './use-stylesheet'
 import useTitle from './use-title'
-import useWebSockets from './use-websockets'
+import useWebSocket from './use-websockets'
 
 export {
   useAsync,
@@ -30,6 +32,7 @@ export {
   useFetch,
   useGet,
   useGraphQL,
+  useKeypressed,
   useKeypress,
   useLocalStorage,
   useMediaQuery,
@@ -39,10 +42,11 @@ export {
   usePageVisibility,
   usePost,
   usePut,
+  useRequest,
   useQuery,
   useScript,
   useStyles,
   useStyleSheet,
   useTitle,
-  useWebSockets,
+  useWebSocket,
 }
